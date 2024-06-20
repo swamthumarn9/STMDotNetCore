89917f35d091039ad77815c98179221a560f5ede
swamthumarn9 <swamthumarn97@gmail.com>
2024-06-20 23:59:07 +0630

    If/Switch Case and sqlInjection

---
+++ STMDotNetCoreWinFormsAppSqlInjection/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace STMDotNetCoreWinFormsAppSqlInjection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLogin = new Button();
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(133, 213);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(112, 34);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(133, 93);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(426, 37);
+            txtEmail.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(133, 148);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(426, 37);
+            txtPassword.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(679, 371);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(btnLogin);
+            Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+    }
+}
---
+++ STMDotNetCoreWinFormsAppSqlInjection/Form1.cs
@@ -0,0 +1,37 @@
+using STMDotNetCore.Shared;
+using System.Configuration;
+
+namespace STMDotNetCoreWinFormsAppSqlInjection
+{
+    public partial class Form1 : Form
+    {
+        private readonly DapperService _dapperService;
+        public Form1()
+        {
+            InitializeComponent();
+            _dapperService = new DapperService(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string query = $"select * from tbl_user where email = @Email and password = @Password";
+            var model = _dapperService.QueryFirstOrDefault<UserModel>(query, new
+            {
+                Email = txtEmail.Text.Trim(),
+                Password = txtPassword.Text.Trim(),
+            });
+            if(model is null)
+            {
+                MessageBox.Show("User doesn't exit.");
+                return;
+            }
+            MessageBox.Show("Is Admin : " + model.Email);
+        }
+    }
+
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
